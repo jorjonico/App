ad119e94e5480cb3c2f61c77cded05efa2ab5e6b
Jorge Matinez <jorjonicodcv@gmail.com>
2022-11-01 11:54:16 -0300

    con lista desde map

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, FlatList, Modal, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 
 export default function App() {
+  const [textItem, setTextItem] = useState('')
+  const [itemList, setItemList] = useState([])
+
+  const onHandleChangeItem = (t) => {
+    setTextItem(t);
+  }
+
+  const addItem = () =>{
+    setItemList (currentItems => [
+      ...currentItems,
+      {id: Math.random().toString(), value:textItem}
+    ])
+    setTextItem('')
+  }
+  
   return (
     <View style={styles.container}>
+      <Text style={styles.titulo}>Lista del Super 🛒</Text>
       <View style={styles.itemContainer}>
-        <TextInput placeholder='Agregar item a la lista' style={styles.inputItem}/>
-        <Button title='Agregar' color= 'cornflowerblue'/>
+        <TextInput value={textItem} placeholder='Agregar item a la lista' style={styles.inputItem} onChangeText={onHandleChangeItem}/>
+        <Button title='Agregar' color= 'cornflowerblue' onPress={addItem}/>
       </View>
-      <View></View>
+      <View>
+        {itemList.map((item) => (
+          <View style={styles.itemstyle}>
+            <Text>{item.value}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -19,9 +42,14 @@ const styles = StyleSheet.create({
     padding: 30, 
     marginTop: 50,
   },
+  titulo:{
+    fontSize: 20,
+    marginLeft: 15,
+    paddingBottom: 20,
+  },
   itemContainer:{
     flexDirection: 'row', 
-    justifyContent: 'center', 
+    justifyContent: 'center',
     alignItems: 'center',
   },
   inputItem:{
@@ -29,5 +57,9 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1, 
     width: 200, 
     marginRight: 20
+  },
+  itemstyle:{
+    marginLeft: 20,
+    paddingTop: 10,
   },
 });
